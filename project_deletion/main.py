f04fd48697a7b06753011bc8dc2df1611342fe2e
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-04-28 20:19:00 -0700

    Update main.py

--- project_deletion/main.py
+++ project_deletion/main.py
@@ -1,19 +1,20 @@
 import os
-
+import sys
 from googleapiclient import discovery
 
 from oauth2client.client import GoogleCredentials
 
 class main:
-    
-    credentials = GoogleCredentials.get_application_default()
+    print 'arguments list:', str(sys.argv)
+    #credentials = GoogleCredentials.get_application_default()
 
-    service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
+    #service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
 
     def services(self):
 
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/sandeep_pochu/sap-coe-devops-test-e30e34abb448.json'
+        #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/sandeep_pochu/sap-coe-devops-test-e30e34abb448.json'
         #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'echo $GOOGLE_APPLICATION_CREDENTIALS'
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = sys.argv[1]
 
         projects = os.getenv("Project_id")
         
