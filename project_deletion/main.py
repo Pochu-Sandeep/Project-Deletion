8ba43ef81259b47299755c8c3dd4c69ebba69c50
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-04-28 20:36:00 -0700

    Update main.py

--- project_deletion/main.py
+++ project_deletion/main.py
@@ -9,7 +9,7 @@ class main:
     def services(self):
         
         #print("arguments list : "+len(sys.argv))
-        print("arguments list : "+str(sys.argv[0]))
+        print("arguments list : "+str(sys.argv[2]))
 
         #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/sandeep_pochu/sap-coe-devops-test-e30e34abb448.json'
         #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'echo $GOOGLE_APPLICATION_CREDENTIALS'
