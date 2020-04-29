bb01d52932b6af4febf00f56d9cb66c7e6ba191f
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-04-28 21:55:53 -0700

    Update computevm.py

--- project_deletion/computevm.py
+++ project_deletion/computevm.py
@@ -30,17 +30,17 @@ class computevm:
                                         
             if len(instance_details) > 0:
                 
-                instance_exists =  True  
-               
-                print("Instances exists in project please delete ........." +project_id)
+                instance_exist =  True  
                 
                 break
-                  
-            else:
+               
+         if instance_exist ==1:
             
-               print(instance_exist)
+            print("Instances exists in project please delete ........." +project_id)
+                  
+         else:
                     
-               print("Instances doesn't exists in the project " +project_id)
+            print("Instances doesn't exists in the project " +project_id)
         
         print(instance_exist)
                        
